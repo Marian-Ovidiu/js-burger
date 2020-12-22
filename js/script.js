a474519554cdd7506008ad2a6daa1c6762532097
Marian Ovidiu Hutanu <h.marian914@gmail.com>
2020-12-22 21:45:48 +0100

    aggiunto controllo "seleziona almeno n ingrediente"

--- js/script.js
+++ js/script.js
@@ -3,28 +3,27 @@ var costi = document.getElementsByClassName('ingredient-container')[0].getElemen
 var totale = document.getElementById('price');
 var inputNomeburger = document.getElementById('nameYourBurger');
 var totalePrezzo = 50;
-var nomeBurger;
-
-
-
-  
-inputNomeburger = '';
-
-
-
+var contatore = 0;
 
 document.getElementById('button-submit').addEventListener('click', function(){
-  nomeBurger.innerText = inputNomeburger;
-  if(nomeBurger === ''){
-    alert('Devi dare un nome al tuo burger');
-  } else {
-    totalePrezzo = 50;
-    for(var i = 0; i < costi.length; i++){
-      if (costi[i].checked){
-        totalePrezzo += parseInt(costi[i].value);
-      }
-    }
-    totale.innerText = totalePrezzo + '$';
-  }
+  contatore = 0;
 
+  for (var j = 0; j < costi.length; j++){
+    if (costi[j].checked){
+      contatore += 1;
+    }
+  }
+  if(inputNomeburger.value.length === 0){
+    alert('Devi dare un nome al tuo burger');
+  } else if (contatore === 0) {
+    alert('Devi selezionare almeno un ingrediente')
+  } else {
+      totalePrezzo = 50;
+      for(var i = 0; i < costi.length; i++){
+        if (costi[i].checked){
+          totalePrezzo += parseInt(costi[i].value);
+        }
+      }
+      totale.innerText = totalePrezzo + '$';
+    }
 })
