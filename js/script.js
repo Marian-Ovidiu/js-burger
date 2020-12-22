c8c5fb6e8d2c897a02dbb67b6401e909ba1bbd9c
Marian Ovidiu Hutanu <h.marian914@gmail.com>
2020-12-22 21:19:25 +0100

    Creato layout html e css e aggiunta funzionalità "crea nome hamburger"

--- js/script.js
+++ js/script.js
@@ -1,13 +1,30 @@
 
 var costi = document.getElementsByClassName('ingredient-container')[0].getElementsByTagName('input');
+var totale = document.getElementById('price');
+var inputNomeburger = document.getElementById('nameYourBurger');
 var totalePrezzo = 50;
+var nomeBurger;
+
+
+
+  
+inputNomeburger = '';
+
+
+
 
 document.getElementById('button-submit').addEventListener('click', function(){
-  totalePrezzo = 50;
-  for(var i = 0; i < costi.length; i++){
-    if (costi[i].checked){
-      totalePrezzo += parseInt(costi[i].value);
+  nomeBurger.innerText = inputNomeburger;
+  if(nomeBurger === ''){
+    alert('Devi dare un nome al tuo burger');
+  } else {
+    totalePrezzo = 50;
+    for(var i = 0; i < costi.length; i++){
+      if (costi[i].checked){
+        totalePrezzo += parseInt(costi[i].value);
+      }
     }
+    totale.innerText = totalePrezzo + '$';
   }
-  alert('totale ' + totalePrezzo);
+
 })
